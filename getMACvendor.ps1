a62404130c4f3993bd40b59d9a648fde04d859df
Steve Frost <9304026+froststevefrost@users.noreply.github.com>
2022-01-12 10:03:24 -0500

    Create getMACvendor.ps1

---
+++ getMACvendor.ps1
@@ -0,0 +1,40 @@
+Clear-Host
+$Listing = @()
+If (Test-Path .\Vendors.csv) {Remove-Item .\Vendors.csv -Force}
+$MACs = Get-Content .\MACs.txt
+$MACs = $MACs -replace '[:]',''
+$i = 0
+ 
+Write-Host "--------------------------------"
+ 
+Foreach ($MAC in $MACs) {
+    $displayMAC = $MAC -replace '(..(?!$))','$1:'  
+ 
+    Write-Progress -Activity "Checking MAC Addresses" -Status "Looking for $displayMAC" -PercentComplete ($i/$MACs.count*100)
+   
+    $URI  = "http://aruljohn.com/mac/$MAC"
+    $HTML = Invoke-WebRequest -Uri $URI
+    Try {
+            $org = (($HTML.ParsedHtml.getElementsByTagName("td")) | Select-Object innerText)[1].innerText
+        }
+    Catch { Write-Host "$displayMAC is not listed or is incorrect."
+            $Object = New-Object -TypeName psobject
+            $Object | Add-Member -MemberType NoteProperty -Name MAC -Value $displayMAC
+            $Object | Add-Member -MemberType NoteProperty -Name Organization -Value $null
+            $Listing += $object
+            Continue
+        }
+ 
+    $Object = New-Object -TypeName psobject
+    $Object | Add-Member -MemberType NoteProperty -Name MAC -Value $displayMAC
+    $Object | Add-Member -MemberType NoteProperty -Name Organization -Value $org
+ 
+    $Listing += $object
+    $i++
+  }
+ 
+Write-Host "--------------------------------"
+ 
+$Listing | Export-CSV -Path .\Vendors.csv -NoTypeInformation -Delimiter ","
+ 
+$Listing
